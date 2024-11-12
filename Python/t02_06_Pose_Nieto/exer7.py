4ad91ac6729b8eac527c44a477b99b95a3ddcef8
BraisPN <a21braispn@iessanclemente.net>
2024-11-12 10:29:18 +0100

    Ejer7

---
+++ Python/t02_06_Pose_Nieto/exer7.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Exercicio 7. Un número de DNI ten o seguinte formato 00000000A.
+
+Escribe un script que lle pida ao usuario o seu DNI e comprobe que sexa correcto. Para iso sigue os seguintes pasos:
+
+Comproba a lonxitude da cadea.
+
+Comproba que os 8 primeiros díxitos son números e o último é unha letra maiúscula. PISTA: utiliza a táboa do código ASCII.
+
+Comproba que a letra introducida é o código de control do DNI. https://www.interior.gob.es/opencms/es/servicios-al-ciudadano/tramites-y-gestiones/dni/calculo-del-digito-de-control-del-nif-nie/#:~:text=Por%20ejemplo%2C%20si%20el%20n%C3%BAmero,n%C3%BAmeros%20y%20d%C3%ADgito%20de%20control.
+
+Por último imprime Válido ou Inválido segundo corresponda.
+
+"""
+
+__author__ = "Brais Pose Nieto"
+
+def comprobacion_dni():
+    
+
+
+
+
+dni = input("Introduzca o seu DNI: ")
