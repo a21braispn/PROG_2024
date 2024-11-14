304477d05ea7e06c96abb28db91b8835713d1a79
BraisPN <a21braispn@iessanclemente.net>
2024-11-14 09:31:28 +0100

    Corrección Formato

--- Python/t02_06_Pose_Nieto/exer8.py
+++ Python/t02_06_Pose_Nieto/exer8.py
@@ -9,40 +9,40 @@ O programa recibirá en orde os seguintes parámetros: tamaño da táboa, númer
 
 __author__ = "Brais Pose Nieto"
 
-def calcular_indice_hash(tam_tabla, num_divisiones, clave):
+def calcular_indice_hash(tam_tabla, num_divisiones, chave):
     """
-    Calcula o índice hash dunha clave empregando o método de folding.
+    Calcula o índice hash dunha chave empregando o método de folding.
 
-    O método divide a clave en partes dun tamaño especificado, suma os valores enteiros de cada parte
+    O método divide a chave en partes dun tamaño especificado, suma os valores enteiros de cada parte
     e calcula o índice como o resto de dividir a suma total entre o tamaño da táboa.
 
     Args:
         tam_tabla (int): Tamaño da táboa hash.
-        num_divisions (int): Número de díxitos en cada división da clave para o folding.
-        clave (str): Clave que se vai converter en índice hash.
+        num_divisions (int): Número de díxitos en cada división da chave para o folding.
+        chave (str): Chave que se vai converter en índice hash.
 
     Returns:
-        int: Índice hash calculado para a clave.
+        int: Índice hash calculado para a chave.
     """
-    suma_partes = 0
-    longitud_clave = len(clave)
+    suma = 0
+    tam_chave = len(chave)
     
     i = 0
-    while i < longitud_clave:
-        parte = clave[i:i+num_divisiones]
+    while i < tam_chave:
+        parte = chave[i:i+num_divisiones]
         suma_partes += int(parte)
         i += num_divisiones
     
-    # Calcular el índice como el resto de dividir la suma por el tamaño de la tabla
-    indice = suma_partes % tam_tabla
+    # Calcula o índice como resto na división da suma polo tamaño da tabla
+    indice = suma % tam_tabla
     
     return indice
 
-# Solicitar parámetros al usuario
-tam_tabla = int(input("Introduce el tamaño de la tabla: "))
-num_divisiones = int(input("Introduce el número de divisiones por folding: "))
-clave = input("Introduce la clave: ")
+# Solicita parámetros ao usuario
+tam_tabla = int(input("Introduce o tamaño da tabla: "))
+num_divisiones = int(input("Introduce o número de divisións por folding: "))
+chave = input("Introduce a chave: ")
 
-# Calcular el índice
-indice = calcular_indice_hash(tam_tabla, num_divisiones, clave)
-print(f"El índice de la clave es: {indice}")
+# Calcula o índice
+indice = calcular_indice_hash(tam_tabla, num_divisiones, chave)
+print(f"O índice da chave é: {indice}")
