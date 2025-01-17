8bd481038d758bdf8b68ca60c9b43263c39b8414
BraisPN <a21braispn@iessanclemente.net>
2025-01-17 12:38:59 +0100

    Nova Tarea Vectores

---
+++ Java/t03_02_Pose_Nieto/exer1.java
@@ -0,0 +1,13 @@
+package t03_02_Pose_Nieto;
+
+import java.util.Scanner;
+
+public class exer1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        scanner.close();
+
+        
+    }
+}
